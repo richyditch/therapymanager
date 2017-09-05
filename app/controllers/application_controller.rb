72388d63870aead80eeccf8dc20238aa16d8784c
richyditch <richard_hilsley@yahoo.com>
2017-09-05 01:20:53 +0100

    new models and part-working user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authorize
+
+
+  protected
+
+    def authorize
+      if request.format == Mime[:html]
+        unless User.find_by(id: session[:user_id])
+          redirect_to login_url, notice: "Please log in"
+        end
+      else
+        authenticate_or_request_with_http_basic do |email, password|
+          user = User.find_by(email: email)
+          user && user.authenticate(password)
+        end
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,40 @@
 Rails.application.routes.draw do
+
+
+  	resources :users
+	root "therapysessions#index"
+	
+	# root to 'index.html'
+
+
+  	controller :sessions do
+	  get  'login' => :new
+	  post 'login' => :create
+  	  delete 'logout' => :destroy
+ 	end
+
+	resources :therapists do
+		resources :clients do
+	  		resources :therapysessions
+	  	end
+	end
+
+	resources :therapists do
+	  	resources :therapysessions
+	end
+
+	resources :clients do
+  		resources :therapysessions
+  	end
+
+    resources :address_books
+	resources :therapysessions
+	resources :clients
+	resources :therapists
+
+# could do this
+	get "/bollocks" => "therapysessions#index"
+  # get 'therapysessions/index'
+  # get 'therapysessions/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
